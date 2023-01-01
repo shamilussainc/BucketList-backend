65345b2d85d4ef420330733c81ac4d12aaee56f2
Shamil Ussain C <shamilussain@gmail.com>
2023-01-01 23:07:45 +0530

    feat: changed settings to single file to make dev env as much as close to prod env.

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -17,6 +17,22 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', False).lower() in ('true')
+
+
+ALLOWED_HOSTS = [host for item in os.getenv('ALLOWED_HOSTS').split(",") if item != ""]
+
 
 # Application definition
 
@@ -91,6 +107,30 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.getenv('DB_NAME'),
+       'USER': os.getenv('DB_USER'),
+       'PASSWORD': os.getenv('DB_PASSWORD'),
+       'HOST': os.getenv('DB_HOST'),
+       'PORT': os.getenv('DB_PORT'),
+   }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase', # This is where you put the name of the db file. 
+#                  # If one doesn't exist, it will be created at migration time.
+#     }
+# }
 
 
 # Static files (CSS, JavaScript, Images)
--- config/settings/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-if os.getenv('ENVIRONMENT') == 'production':
-    from .production import *
-else:
-    from .development import *
